d82f2262b856277a58386bc23ddf238a50755097
OCamlPro-Iguernlala <mohamed.iguernlala@ocamlpro.com>
2018-10-30 19:17:37 +0100

    update year in copyright

--- sources/lib/util/myDynlink.ml
+++ sources/lib/util/myDynlink.ml
@@ -1,7 +1,7 @@
 (******************************************************************************)
 (*                                                                            *)
 (*     Alt-Ergo: The SMT Solver For Software Verification                     *)
-(*     Copyright (C) 2013-2017 --- OCamlPro SAS                               *)
+(*     Copyright (C) 2013-2018 --- OCamlPro SAS                               *)
 (*                                                                            *)
 (*     This file is distributed under the terms of the license indicated      *)
 (*     in the file 'License.OCamlPro'. If 'License.OCamlPro' is not           *)
