935a41e057edf8d3efdda70778796ab5d5f0e3d4
OCamlPro-Iguernlala <mohamed.iguernlala@ocamlpro.com>
2018-12-02 09:39:17 +0100

    improve indentation checker script

--- extra/check_indentation.sh
+++ extra/check_indentation.sh
@@ -2,13 +2,15 @@
 
 files=`find ../sources -regex .*[.]ml[i]?`
 
+before=`git diff`
+
 for f in $files ; do
     ocp-indent -i $f
 done
 
 res=`git diff`
 
-if [ "$res" = "" ] ; then
+if [ "$res" = "$before" ] ; then
     echo "success: ml(i) files are well indented with ocp-indent"
 else
     echo "failure: some ml(i) files are not well indented with ocp-indent"
