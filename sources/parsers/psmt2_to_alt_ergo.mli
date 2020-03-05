e25b0c6959b84bb6123498b280bc1f520f4a957f
Albin Coquereau <albin.coquereau@ocamlpro.com>
2020-03-05 17:39:26 +0100

    Update alt-ergo-parsers and plugins documentation

--- sources/parsers/psmt2_to_alt_ergo.mli
+++ sources/parsers/psmt2_to_alt_ergo.mli
@@ -8,4 +8,11 @@
 (*                                                                            *)
 (******************************************************************************)
 
-(* empty interface *)
+(** {1 Interface Module with the library psmt2-frontend} *)
+
+(** Offer an interface with the library
+    {{:https://github.com/OCamlPro-Coquera/psmt2-frontend}[psmt2-frontend]}
+    and register a parser for smt2 and psmt2 extensions. This interface allows
+    Alt-Ergo to partially support the SMT-LIB2 standard and a polymorphic
+    extension.
+*)
