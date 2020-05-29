efcb9434b0f91b1bffd4266d48890eae95810a02
Albin Coquereau <albin.coquereau@ocamlpro.com>
2020-05-29 11:17:50 +0200

    build ocpchecker with dune

--- rsc/extra/check_style.sh
+++ rsc/extra/check_style.sh
@@ -8,10 +8,10 @@
 
 # Cd to the extra dir regardless of where the script was called
 git_repo=`git rev-parse --show-toplevel`
-cd $git_repo/rsc/extra
+cd $git_repo/rsc/extra/ocpchecker
 
 # Compile style checker
-ocamlfind ocamlopt -o ocp-checker -linkpkg -package stdlib-shims ocpChecker.ml
+make bin
 
 # List source files to check
 files=`find $git_repo/src -regex .*[.]ml[ily]?`
@@ -25,7 +25,7 @@ for f in $files ; do
 	status=`git status $f -s --ignored`
 	res=`echo $status | cut -d' ' -f1`
 	if ! [ "$res" = "??" ] && ! [ "$res" = "!!" ] ; then
-		$git_repo/rsc/extra/ocp-checker $f
+		$git_repo/rsc/extra/ocpchecker/ocp-checker $f
 		if ! [ "$?" -eq "0" ] ; then
 			cpt=$((cpt+1))
 		fi
