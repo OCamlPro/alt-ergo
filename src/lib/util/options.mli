1c3bc3089634e58f65b1b4cd2f8d28512a5a6018
Albin Coquereau <6535385+OCamlPro-Coquera@users.noreply.github.com>
2020-06-04 10:22:59 +0200

    Update src/lib/util/options.mli
    Co-authored-by: Mattias <mattias@ocamlpro.com>

--- src/lib/util/options.mli
+++ src/lib/util/options.mli
@@ -985,7 +985,7 @@ val set_std_fmt : Format.formatter -> unit
 val set_err_fmt : Format.formatter -> unit
 (** Default to [Format.err_formatter] *)
 
-(** Value specifying the formatter use to output results *)
+(** Value specifying the formatter used to output results *)
 val get_fmt_std : unit -> Format.formatter
 (** Default to [std_fmt] *)
 
