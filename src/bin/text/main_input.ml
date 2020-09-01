35c42398a4f20e517e18e788d727abbd9c74aed3
Albin Coquereau <albin.coquereau@ocamlpro.com>
2020-09-01 11:13:31 +0200

    first prototype for models in Why3, lots of hard coded stuff that needs to be well understood before making it work for every case

--- src/bin/text/main_input.ml
+++ src/bin/text/main_input.ml
@@ -15,7 +15,7 @@ open AltErgoParsers
 (* === LEGACY input method === *)
 
 let () =
-  let module M : Input.S = struct
+  let module M : Input.S with type parsed = Parsed.decl = struct
 
     (* Parsing *)
 
