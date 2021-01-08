98f5b07a5a58d16815ba7e2cb42bdeebf508a806
Albin Coquereau <pro.acoquer@protonmail.com>
2021-01-08 18:42:08 +0100

    Move profile module from Models module to structures

--- src/lib/frontend/models.ml
+++ src/lib/frontend/models.ml
@@ -23,66 +23,6 @@ module ME = Expr.Map
 module SE = Expr.Set
 module MS = Map.Make(String)
 
-module Profile = struct
-
-  module P = Map.Make
-      (struct
-        type t = Sy.t * Ty.t list * Ty.t
-
-        let (|||) c1 c2 = if c1 <> 0 then c1 else c2
-
-        let compare (a1, b1, c1)  (a2, b2, c2) =
-          let l1_l2 = List.length b1 - List.length b2 in
-          let c = l1_l2 ||| (Ty.compare c1 c2) ||| (Sy.compare a1 a2) in
-          if c <> 0 then c
-          else
-            let c = ref 0 in
-            try
-              List.iter2
-                (fun ty1 ty2 ->
-                   let d = Ty.compare ty1 ty2 in
-                   if d <> 0 then begin c := d; raise Exit end
-                ) b1 b2;
-              0
-            with
-            | Exit -> assert (!c <> 0); !c
-            | Invalid_argument _ -> assert false
-      end)
-
-  module V = Set.Make
-      (struct
-        type t = (E.t * (X.r * string)) list * (X.r * string)
-        let compare (l1, (v1,_)) (l2, (v2,_)) =
-          let c = X.hash_cmp v1 v2 in
-          if c <> 0 then c
-          else
-            let c = ref 0 in
-            try
-              List.iter2
-                (fun (_,(x,_)) (_,(y,_)) ->
-                   let d = X.hash_cmp x y in
-                   if d <> 0 then begin c := d; raise Exit end
-                ) l1 l2;
-              !c
-            with
-            | Exit -> !c
-            | Invalid_argument _ -> List.length l1 - List.length l2
-      end)
-
-  let add p v mp =
-    let prof_p = try P.find p mp with Not_found -> V.empty in
-    if V.mem v prof_p then mp
-    else P.add p (V.add v prof_p) mp
-
-  let iter = P.iter
-
-  let fold = P.fold
-
-  let empty = P.empty
-
-  let is_empty = P.is_empty
-end
-
 let constraints = ref MS.empty
 
 module Pp_smtlib_term = struct
@@ -394,7 +334,8 @@ module SmtlibCounterExample = struct
            let representants =
              Profile.V.fold (fun (xs_values,(_rep,srep)) acc ->
                  assert ((List.length xs_ty_named) = (List.length xs_values));
-                 records := check_records !records xs_ty_named xs_values f ty srep;
+                 records :=
+                   check_records !records xs_ty_named xs_values f ty srep;
                  let reps = try MS.find srep acc with Not_found -> [] in
                  MS.add srep (xs_values :: reps) acc
                ) st MS.empty in
@@ -478,21 +419,21 @@ end
 
 let output_concrete_model fmt props functions constants arrays =
   if get_interpretation () then begin
-      Printer.print_fmt ~flushed:false fmt "@[<v 0>unknown@ ";
-      Printer.print_fmt ~flushed:false fmt "@[<v 2>(model@,";
-      if Options.get_output_format () == Why3 then begin
-        Why3CounterExample.output_constraints fmt props
-      end;
-
-      fprintf fmt "@ ; Functions@ ";
-      let records = SmtlibCounterExample.output_functions_counterexample
-          fmt  MS.empty functions in
-
-      fprintf fmt "@ ; Constants@ ";
-      SmtlibCounterExample.output_constants_counterexample
-        fmt records constants;
-
-      SmtlibCounterExample.output_arrays_counterexample fmt arrays;
-
-      Printer.print_fmt fmt "@]@ )";
+    Printer.print_fmt ~flushed:false fmt "@[<v 0>unknown@ ";
+    Printer.print_fmt ~flushed:false fmt "@[<v 2>(model@,";
+    if Options.get_output_format () == Why3 then begin
+      Why3CounterExample.output_constraints fmt props
     end;
+
+    fprintf fmt "@ ; Functions@ ";
+    let records = SmtlibCounterExample.output_functions_counterexample
+        fmt  MS.empty functions in
+
+    fprintf fmt "@ ; Constants@ ";
+    SmtlibCounterExample.output_constants_counterexample
+      fmt records constants;
+
+    SmtlibCounterExample.output_arrays_counterexample fmt arrays;
+
+    Printer.print_fmt fmt "@]@ )";
+  end;
