971210349bd20bf4fb620fa47b63cdb35d312ed9
Albin Coquereau <pro.acoquer@protonmail.com>
2021-01-08 11:24:22 +0100

    Remove deadcode from models and mv models.ml in frontend

--- src/lib/frontend/models.ml
+++ src/lib/frontend/models.ml
@@ -24,27 +24,6 @@ module SE = Expr.Set
 
 module Sorts = Map.Make(String)
 
-let h = ref Sorts.empty
-
-let sorts parsed =
-  let open Parsed in
-  Format.eprintf "@[<v 2>";
-  Seq.iter (fun d -> match d with
-      | Parsed.Axiom (_, _, _, le) -> begin
-          match le.pp_desc with
-          | PPapp("sort", f)
-          | PPforall_named (_, _, _, {pp_desc = PPapp("sort", f); _}) ->
-            begin
-              match f with
-              | [{pp_desc = PPapp(t, _); _}; {pp_desc = PPapp(f, args); _}] ->
-                h := Sorts.add f (List.length args, t) !h
-              | _ -> ()
-            end
-          | _ -> ()
-        end
-      | _ -> ()
-    ) parsed
-
 module Profile = struct
 
   module P = Map.Make
