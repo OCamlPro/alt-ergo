3b4d843a147b5a02da498389d2e4dbf9f75e741b
OriginLabs-Iguernlala <mohamed.iguernlala@origin-labs.com>
2021-01-08 19:46:18 +0100

    clean unused code

--- src/lib/reasoners/uf.mli
+++ src/lib/reasoners/uf.mli
@@ -57,7 +57,6 @@ val class_of : t -> Expr.t -> Expr.t list
 val rclass_of : t -> r -> Expr.Set.t
 
 val cl_extract : t -> Expr.Set.t list
-val model : t -> (r * Expr.t list * (Expr.t * r) list) list * (Expr.t list) list
 
 val print : t -> unit
 val term_repr : t -> Expr.t -> Expr.t
