150f134aff194ea0cb7bf02f2881fa0e57aa2e12
Guillaume Bury <guillaume.bury@gmail.com>
2021-06-30 14:52:18 +0200

    Fix non regression script

--- non-regression/main_script.sh
+++ non-regression/main_script.sh
@@ -38,26 +38,31 @@ main_script_out=$(mktemp)
 main_script_err=$(mktemp)
 trap "rm $main_script_out $main_script_err" EXIT
 
+## Finding files
+################################################################################################
+
+files_pat=("-name" "*.ae" "-or" "-name" "*.smt2" "-or" "-name" "*.zip")
+
 ## Challenges
 ################################################################################################
 
 score=0
 total=0
 
-big_total=`ls challenges/*/*.ae | wc -l`
+big_total=`find challenges "${files_pat[@]}" | wc -l`
 for kind in `ls challenges/`
 do
     echo ""
     echo -n "challenges/$kind"
-    for ae in `ls challenges/$kind/*.ae`
+    for ae in `find challenges/$kind/ "${files_pat[@]}"`
     do
         tput hpa 25
         total=`expr $total + 1`
         echo -n "$total / $big_total"
-        timeout 2 $pr $ae $opt 1> $main_script_out 2> $main_script_err
+        timeout 2 $pr -o native $ae $opt 1> $main_script_out 2> $main_script_err
         if grep -q -w Valid $main_script_out ; then
 	    score=`expr $score + 1`
-            echo "\e[32m    > [OK] ../non-regression/$ae\e[39m"
+            echo -e "\e[32m    > [OK] ../non-regression/$ae\e[39m"
         else
             echo ../non-regression/$ae >> main_script.log
             cat $main_script_out >> main_script.log
@@ -82,17 +87,17 @@ echo "$limit"
 score=0
 total=0
 
-big_total=`ls valid/*/*.ae | wc -l`
+big_total=`find valid "${files_pat[@]}" | wc -l`
 for kind in `ls valid/`
 do
     echo ""
     echo -n "valid/$kind"
-    for ae in `ls valid/$kind/*.ae`
+    for ae in `find valid/$kind/ "${files_pat[@]}"`
     do
         tput hpa 25
         total=`expr $total + 1`
         echo -n "$total / $big_total"
-        timeout 2 $pr $ae $opt 1> $main_script_out 2> $main_script_err
+        timeout 2 $pr -o native $ae $opt 1> $main_script_out 2> $main_script_err
         if grep -q -w Valid $main_script_out ; then
 	    score=`expr $score + 1`
         else
@@ -119,17 +124,17 @@ echo "$limit"
 score=0
 total=0
 
-big_total=`ls invalid/*/*.ae | wc -l`
+big_total=`find invalid "${files_pat[@]}" | wc -l`
 for kind in `ls invalid/`
 do
     echo ""
     echo -n "invalid/$kind"
-    for ae in `ls invalid/$kind/*.ae`
+    for ae in `find invalid/$kind/ "${files_pat[@]}"`
     do
         tput hpa 25
         total=`expr $total + 1`
         echo -n "$total / $big_total"
-        timeout 2 $pr $ae $opt 1> $main_script_out 2> $main_script_err
+        timeout 2 $pr -o native $ae $opt 1> $main_script_out 2> $main_script_err
         if grep -q -w "I don't know" $main_script_out ; then
 	    score=`expr $score + 1`
         else
@@ -156,17 +161,17 @@ echo "$limit"
 score=0
 total=0
 
-big_total=`ls incorrect/*/*.ae | wc -l`
+big_total=`find incorrect "${files_pat[@]}" | wc -l`
 for kind in `ls incorrect/`
 do
     echo ""
     echo -n "incorrect/$kind"
-    for ae in `ls incorrect/$kind/*.ae`
+    for ae in `find incorrect/$kind "${files_pat[@]}"`
     do
         tput hpa 25
         total=`expr $total + 1`
         echo -n "$total / $big_total"
-        timeout 2 $pr $ae $opt 1> $main_script_out 2> $main_script_err
+        timeout 2 $pr -o native $ae $opt 1> $main_script_out 2> $main_script_err
         if [ $? -eq 0 ] ; then
             echo ../non-regression/$ae >> main_script.log
             cat $main_script_out >> main_script.log
