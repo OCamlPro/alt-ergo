9350511f4a7a37cc821dc61f7672410c901eb963
hra687261 <hra687261@gmail.com>
2022-11-16 20:47:28 +0100

    Update `reinit_ctx` comment

--- src/lib/reasoners/sat_solver_sig.ml
+++ src/lib/reasoners/sat_solver_sig.ml
@@ -70,8 +70,7 @@ module type S = sig
 
   val reset_refs : unit -> unit
 
-  (** [reinit_ctx ()] reinitializes the solving context.
-      The order in which the calls are done is important. *)
+  (** [reinit_ctx ()] reinitializes the solving context. *)
   val reinit_ctx : unit -> unit
 
 end
