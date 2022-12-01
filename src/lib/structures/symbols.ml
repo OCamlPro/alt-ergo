cfdc501cea0f7c91916a00bddfbca513350e0cf8
Pierre Villemot <tiky.halbaroth@gmail.com>
2022-12-01 12:40:58 +0100

    Try ppx_deriving_inline

--- src/lib/structures/symbols.ml
+++ src/lib/structures/symbols.ml
@@ -29,7 +29,25 @@
 type builtin =
     LE | LT (* arithmetic *)
   | IsConstr of Hstring.t (* ADT tester *)
-[@@deriving compare]
+[@@deriving_inline compare]
+let _ = fun (_ : builtin) -> ()
+let compare_builtin =
+  (fun a__001_ ->
+     fun b__002_ ->
+       if Ppx_compare_lib.phys_equal a__001_ b__002_
+       then 0
+       else
+         (match (a__001_, b__002_) with
+          | (LE, LE) -> 0
+          | (LE, _) -> (-1)
+          | (_, LE) -> 1
+          | (LT, LT) -> 0
+          | (LT, _) -> (-1)
+          | (_, LT) -> 1
+          | (IsConstr _a__003_, IsConstr _b__004_) ->
+            Hstring.compare _a__003_ _b__004_) : builtin -> builtin -> int)
+let _ = compare_builtin
+[@@@end]
 
 type operator =
     Plus | Minus | Mult | Div | Modulo
@@ -42,7 +60,116 @@ type operator =
   | Constr of Hstring.t (* enums, adts *)
   | Destruct of Bool.t * Hstring.t
   | Tite
-[@@deriving compare]
+[@@deriving_inline compare]
+let _ = fun (_ : operator) -> ()
+let compare_operator =
+  (fun a__005_ ->
+     fun b__006_ ->
+       if Ppx_compare_lib.phys_equal a__005_ b__006_
+       then 0
+       else
+         (match (a__005_, b__006_) with
+          | (Plus, Plus) -> 0
+          | (Plus, _) -> (-1)
+          | (_, Plus) -> 1
+          | (Minus, Minus) -> 0
+          | (Minus, _) -> (-1)
+          | (_, Minus) -> 1
+          | (Mult, Mult) -> 0
+          | (Mult, _) -> (-1)
+          | (_, Mult) -> 1
+          | (Div, Div) -> 0
+          | (Div, _) -> (-1)
+          | (_, Div) -> 1
+          | (Modulo, Modulo) -> 0
+          | (Modulo, _) -> (-1)
+          | (_, Modulo) -> 1
+          | (Concat, Concat) -> 0
+          | (Concat, _) -> (-1)
+          | (_, Concat) -> 1
+          | (Extract, Extract) -> 0
+          | (Extract, _) -> (-1)
+          | (_, Extract) -> 1
+          | (Get, Get) -> 0
+          | (Get, _) -> (-1)
+          | (_, Get) -> 1
+          | (Set, Set) -> 0
+          | (Set, _) -> (-1)
+          | (_, Set) -> 1
+          | (Fixed, Fixed) -> 0
+          | (Fixed, _) -> (-1)
+          | (_, Fixed) -> 1
+          | (Float, Float) -> 0
+          | (Float, _) -> (-1)
+          | (_, Float) -> 1
+          | (Reach, Reach) -> 0
+          | (Reach, _) -> (-1)
+          | (_, Reach) -> 1
+          | (Access _a__007_, Access _b__008_) ->
+            Hstring.compare _a__007_ _b__008_
+          | (Access _, _) -> (-1)
+          | (_, Access _) -> 1
+          | (Record, Record) -> 0
+          | (Record, _) -> (-1)
+          | (_, Record) -> 1
+          | (Sqrt_real, Sqrt_real) -> 0
+          | (Sqrt_real, _) -> (-1)
+          | (_, Sqrt_real) -> 1
+          | (Abs_int, Abs_int) -> 0
+          | (Abs_int, _) -> (-1)
+          | (_, Abs_int) -> 1
+          | (Abs_real, Abs_real) -> 0
+          | (Abs_real, _) -> (-1)
+          | (_, Abs_real) -> 1
+          | (Real_of_int, Real_of_int) -> 0
+          | (Real_of_int, _) -> (-1)
+          | (_, Real_of_int) -> 1
+          | (Int_floor, Int_floor) -> 0
+          | (Int_floor, _) -> (-1)
+          | (_, Int_floor) -> 1
+          | (Int_ceil, Int_ceil) -> 0
+          | (Int_ceil, _) -> (-1)
+          | (_, Int_ceil) -> 1
+          | (Sqrt_real_default, Sqrt_real_default) -> 0
+          | (Sqrt_real_default, _) -> (-1)
+          | (_, Sqrt_real_default) -> 1
+          | (Sqrt_real_excess, Sqrt_real_excess) -> 0
+          | (Sqrt_real_excess, _) -> (-1)
+          | (_, Sqrt_real_excess) -> 1
+          | (Min_real, Min_real) -> 0
+          | (Min_real, _) -> (-1)
+          | (_, Min_real) -> 1
+          | (Min_int, Min_int) -> 0
+          | (Min_int, _) -> (-1)
+          | (_, Min_int) -> 1
+          | (Max_real, Max_real) -> 0
+          | (Max_real, _) -> (-1)
+          | (_, Max_real) -> 1
+          | (Max_int, Max_int) -> 0
+          | (Max_int, _) -> (-1)
+          | (_, Max_int) -> 1
+          | (Integer_log2, Integer_log2) -> 0
+          | (Integer_log2, _) -> (-1)
+          | (_, Integer_log2) -> 1
+          | (Pow, Pow) -> 0
+          | (Pow, _) -> (-1)
+          | (_, Pow) -> 1
+          | (Integer_round, Integer_round) -> 0
+          | (Integer_round, _) -> (-1)
+          | (_, Integer_round) -> 1
+          | (Constr _a__009_, Constr _b__010_) ->
+            Hstring.compare _a__009_ _b__010_
+          | (Constr _, _) -> (-1)
+          | (_, Constr _) -> 1
+          | (Destruct (_a__011_, _a__013_), Destruct (_b__012_, _b__014_)) ->
+            (match Bool.compare _a__011_ _b__012_ with
+             | 0 -> Hstring.compare _a__013_ _b__014_
+             | n -> n)
+          | (Destruct _, _) -> (-1)
+          | (_, Destruct _) -> 1
+          | (Tite, Tite) -> 0) : operator -> operator -> int)
+let _ = compare_operator
+[@@@end]
 
 type lit =
   (* literals *)
@@ -51,7 +178,34 @@ type lit =
   | L_neg_eq
   | L_neg_built of builtin
   | L_neg_pred
-[@@deriving compare]
+[@@deriving_inline compare]
+let _ = fun (_ : lit) -> ()
+let compare_lit =
+  (fun a__015_ ->
+     fun b__016_ ->
+       if Ppx_compare_lib.phys_equal a__015_ b__016_
+       then 0
+       else
+         (match (a__015_, b__016_) with
+          | (L_eq, L_eq) -> 0
+          | (L_eq, _) -> (-1)
+          | (_, L_eq) -> 1
+          | (L_built _a__017_, L_built _b__018_) ->
+            compare_builtin _a__017_ _b__018_
+          | (L_built _, _) -> (-1)
+          | (_, L_built _) -> 1
+          | (L_neg_eq, L_neg_eq) -> 0
+          | (L_neg_eq, _) -> (-1)
+          | (_, L_neg_eq) -> 1
+          | (L_neg_built _a__019_, L_neg_built _b__020_) ->
+            compare_builtin _a__019_ _b__020_
+          | (L_neg_built _, _) -> (-1)
+          | (_, L_neg_built _) -> 1
+          | (L_neg_pred, L_neg_pred) -> 0) : lit -> lit -> int)
+let _ = compare_lit
+[@@@end]
+
+
 
 type form =
   (* formulas *)
@@ -61,22 +215,118 @@ type form =
   | F_Xor
   | F_Lemma
   | F_Skolem
-[@@deriving compare]
+[@@deriving_inline compare]
+let _ = fun (_ : form) -> ()
+let compare_form =
+  (fun a__021_ ->
+     fun b__022_ ->
+       if Ppx_compare_lib.phys_equal a__021_ b__022_
+       then 0
+       else
+         (match (a__021_, b__022_) with
+          | (F_Unit _a__023_, F_Unit _b__024_) ->
+            Bool.compare _a__023_ _b__024_
+          | (F_Unit _, _) -> (-1)
+          | (_, F_Unit _) -> 1
+          | (F_Clause _a__025_, F_Clause _b__026_) ->
+            Bool.compare _a__025_ _b__026_
+          | (F_Clause _, _) -> (-1)
+          | (_, F_Clause _) -> 1
+          | (F_Iff, F_Iff) -> 0
+          | (F_Iff, _) -> (-1)
+          | (_, F_Iff) -> 1
+          | (F_Xor, F_Xor) -> 0
+          | (F_Xor, _) -> (-1)
+          | (_, F_Xor) -> 1
+          | (F_Lemma, F_Lemma) -> 0
+          | (F_Lemma, _) -> (-1)
+          | (_, F_Lemma) -> 1
+          | (F_Skolem, F_Skolem) -> 0) : form -> form -> int)
+let _ = compare_form
+[@@@end]
 
 type name_kind =
   | Ac
   | Other
-[@@deriving compare]
+[@@deriving_inline compare]
+let _ = fun (_ : name_kind) -> ()
+let compare_name_kind =
+  (Ppx_compare_lib.polymorphic_compare : name_kind -> name_kind -> int)
+let _ = compare_name_kind
+[@@@end]
 
 type bound_kind = VarBnd of Var.t | ValBnd of Numbers.Q.t
-[@@deriving equal, compare]
+[@@deriving_inline equal, compare]
+let _ = fun (_ : bound_kind) -> ()
+let equal_bound_kind =
+  (fun a__029_ ->
+     fun b__030_ ->
+       if Ppx_compare_lib.phys_equal a__029_ b__030_
+       then true
+       else
+         (match (a__029_, b__030_) with
+          | (VarBnd _a__031_, VarBnd _b__032_) -> Var.equal _a__031_ _b__032_
+          | (VarBnd _, _) -> false
+          | (_, VarBnd _) -> false
+          | (ValBnd _a__033_, ValBnd _b__034_) ->
+            Numbers.Q.equal _a__033_ _b__034_) : bound_kind ->
+       bound_kind -> bool)
+let _ = equal_bound_kind
+let compare_bound_kind =
+  (fun a__035_ ->
+     fun b__036_ ->
+       if Ppx_compare_lib.phys_equal a__035_ b__036_
+       then 0
+       else
+         (match (a__035_, b__036_) with
+          | (VarBnd _a__037_, VarBnd _b__038_) ->
+            Var.compare _a__037_ _b__038_
+          | (VarBnd _, _) -> (-1)
+          | (_, VarBnd _) -> 1
+          | (ValBnd _a__039_, ValBnd _b__040_) ->
+            Numbers.Q.compare _a__039_ _b__040_) : bound_kind ->
+       bound_kind -> int)
+let _ = compare_bound_kind
+[@@@end]
 
 type bound = {
   sort: Ty.t;
   is_open: Bool.t;
   is_lower: Bool.t;
   kind: bound_kind;
-} [@@deriving equal, compare]
+} [@@deriving_inline equal, compare]
+let _ = fun (_ : bound) -> ()
+let equal_bound =
+  (fun a__041_ ->
+     fun b__042_ ->
+       if Ppx_compare_lib.phys_equal a__041_ b__042_
+       then true
+       else
+         Ppx_compare_lib.(&&) (Ty.equal a__041_.sort b__042_.sort)
+           (Ppx_compare_lib.(&&) (Bool.equal a__041_.is_open b__042_.is_open)
+              (Ppx_compare_lib.(&&)
+                 (Bool.equal a__041_.is_lower b__042_.is_lower)
+                 (equal_bound_kind a__041_.kind b__042_.kind))) : bound ->
+       bound ->
+       bool)
+let _ = equal_bound
+let compare_bound =
+  (fun a__043_ ->
+     fun b__044_ ->
+       if Ppx_compare_lib.phys_equal a__043_ b__044_
+       then 0
+       else
+         (match Ty.compare a__043_.sort b__044_.sort with
+          | 0 ->
+            (match Bool.compare a__043_.is_open b__044_.is_open with
+             | 0 ->
+               (match Bool.compare a__043_.is_lower b__044_.is_lower with
+                | 0 -> compare_bound_kind a__043_.kind b__044_.kind
+                | n -> n)
+             | n -> n)
+          | n -> n) : bound -> bound -> int)
+let _ = compare_bound
+[@@@end]
 
 type t =
   | True
@@ -93,7 +343,66 @@ type t =
   | In of bound * bound
   | MapsTo of Var.t
   | Let
-[@@deriving compare]
+[@@deriving_inline compare]
+let _ = fun (_ : t) -> ()
+let compare =
+  (fun a__045_ ->
+     fun b__046_ ->
+       if Ppx_compare_lib.phys_equal a__045_ b__046_
+       then 0
+       else
+         (match (a__045_, b__046_) with
+          | (True, True) -> 0
+          | (True, _) -> (-1)
+          | (_, True) -> 1
+          | (False, False) -> 0
+          | (False, _) -> (-1)
+          | (_, False) -> 1
+          | (Void, Void) -> 0
+          | (Void, _) -> (-1)
+          | (_, Void) -> 1
+          | (Int _a__047_, Int _b__048_) -> Hstring.compare _a__047_ _b__048_
+          | (Int _, _) -> (-1)
+          | (_, Int _) -> 1
+          | (Real _a__049_, Real _b__050_) ->
+            Hstring.compare _a__049_ _b__050_
+          | (Real _, _) -> (-1)
+          | (_, Real _) -> 1
+          | (Var _a__051_, Var _b__052_) -> Var.compare _a__051_ _b__052_
+          | (Var _, _) -> (-1)
+          | (_, Var _) -> 1
+          | (Name (_a__053_, _a__055_), Name (_b__054_, _b__056_)) ->
+            (match Hstring.compare _a__053_ _b__054_ with
+             | 0 -> compare_name_kind _a__055_ _b__056_
+             | n -> n)
+          | (Name _, _) -> (-1)
+          | (_, Name _) -> 1
+          | (Bitv _a__057_, Bitv _b__058_) ->
+            String.compare _a__057_ _b__058_
+          | (Bitv _, _) -> (-1)
+          | (_, Bitv _) -> 1
+          | (Op _a__059_, Op _b__060_) -> compare_operator _a__059_ _b__060_
+          | (Op _, _) -> (-1)
+          | (_, Op _) -> 1
+          | (Lit _a__061_, Lit _b__062_) -> compare_lit _a__061_ _b__062_
+          | (Lit _, _) -> (-1)
+          | (_, Lit _) -> 1
+          | (Form _a__063_, Form _b__064_) -> compare_form _a__063_ _b__064_
+          | (Form _, _) -> (-1)
+          | (_, Form _) -> 1
+          | (In (_a__065_, _a__067_), In (_b__066_, _b__068_)) ->
+            (match compare_bound _a__065_ _b__066_ with
+             | 0 -> compare_bound _a__067_ _b__068_
+             | n -> n)
+          | (In _, _) -> (-1)
+          | (_, In _) -> 1
+          | (MapsTo _a__069_, MapsTo _b__070_) ->
+            Var.compare _a__069_ _b__070_
+          | (MapsTo _, _) -> (-1)
+          | (_, MapsTo _) -> 1
+          | (Let, Let) -> 0) : t -> t -> int)
+let _ = compare
+[@@@end]
 
 type s = t
 
