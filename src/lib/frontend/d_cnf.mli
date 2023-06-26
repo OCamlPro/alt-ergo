fcd570ee34ae1442fc451c0933c36ad2786e8ce9
Basile Clément <129742207+bclement-ocp@users.noreply.github.com>
2023-06-26 14:55:12 +0200

    Add support for `check_sat` in the Dolmen frontend (#660)
    Rebased and cleaned version of #594.

--- src/lib/frontend/d_cnf.mli
+++ src/lib/frontend/d_cnf.mli
@@ -34,9 +34,10 @@ val clear_cache : unit -> unit
 val make :
   D_loop.DStd.Loc.file ->
   Commands.sat_tdecl list ->
-  [ D_loop.Typer_Pipe.typechecked
-  | `Goal of Dolmen.Std.Expr.term
-  ] D_loop.Typer_Pipe.stmt ->
+  [< D_loop.Typer_Pipe.typechecked
+  | `Goal  of Dolmen.Std.Expr.term
+  | `Check of Dolmen.Std.Expr.term
+         > `Hyp ] D_loop.Typer_Pipe.stmt ->
   Commands.sat_tdecl list
 (** [make acc stmt] Makes one or more [Commands.sat_tdecl] from the
     type-checked statement [stmt] and appends them to [acc].
