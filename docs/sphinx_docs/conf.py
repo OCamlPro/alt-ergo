33b13a36ed7b6d7478f1adba0ebaca2b3a67060d
Pierre Villemot <tiky.halbaroth@gmail.com>
2023-07-27 10:38:56 +0200

    Keep the default width of the rtd-theme

--- docs/sphinx_docs/conf.py
+++ docs/sphinx_docs/conf.py
@@ -56,9 +56,6 @@ myst_heading_anchors = 3
 
 html_show_sourcelink = False
 
-def setup(app):
-    app.add_css_file('css/dac_theme_modif.css')
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
