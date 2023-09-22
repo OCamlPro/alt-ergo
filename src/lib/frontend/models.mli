4b03759cf2c9dd8b5d64254c1e1e2ed9aa9ecca1
Pierre Villemot <tiky.halbaroth@gmail.com>
2023-09-22 14:19:35 +0200

    Update documentation about `output_concrete_model`
    I also remove the mention about the different kind of formats used
    to print models. Indeed, we use only the SMT-LIB format and
    the Why3 format is slightly different but probably outdated.

--- src/lib/frontend/models.mli
+++ src/lib/frontend/models.mli
@@ -37,10 +37,6 @@ type t = {
   arrays : ModelMap.t;
 }
 
-(** Print the given counterexample on the given formatter with the
-    corresponding format set with Options.get_output_format.
-    - functions: the functions of the model;
-    - constants: the variables of the model;
-    - arrays: (experimental) the arrays of the model.
-*)
 val output_concrete_model : Format.formatter -> t -> unit
+(** [output_concrete_model ppf mdl] prints the model [mdl] on
+    the formatter [ppf]. *)
