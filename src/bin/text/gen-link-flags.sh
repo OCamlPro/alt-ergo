0e035d7ad07b6206c1c7f83e24b9bebc398842e3
Basile Clément <basile.clement@ocamlpro.com>
2024-02-22 09:25:59 +0100

    feat: Statically linked binary releases
    This patch modifies the Docker workflow to build a statically linked
    portable binary of Alt-Ergo, following [1]. This also adds static builds
    for macOS on both x86-64 and arm64 platforms.
    
    Building the Linux binaries is fairly quick and hence performed on
    push/pull_request. It can also be manually requested. macOS static
    builds are much slower and can only be manually requested.
    
    [1] : https://ocamlpro.com/blog/2021_09_02_generating_static_and_portable_executables_with_ocaml/

---
+++ src/bin/text/gen-link-flags.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+set -ue
+
+LINK_MODE="$1"
+OS="$2"
+FLAGS=
+CCLIB=
+
+case "$LINK_MODE" in
+  dynamic)
+    ;; # No extra flags needed
+  static)
+    case "$OS" in
+      linux)
+        CCLIB="-static -no-pie";;
+      *)
+        echo "No known static compilation flags for '$OS'" >&2
+        exit 1
+    esac;;
+  mixed)
+    FLAGS="-noautolink"
+    # Note: for OCaml 5, use -lcamlstrnat and -lunixnat and mind zlib
+    # https://github.com/ocaml/ocaml/issues/12562
+    CCLIB="-lstdcompat_stubs -lcamlzip -lnums -lzarith -lcamlstr -lunix -lz"
+    LIBS="gmp"
+    case "$OS" in
+      linux)
+        for lib in $LIBS; do
+          CCLIB="$CCLIB -l$lib"
+        done
+	CCLIB="-Wl,-Bstatic $CCLIB -Wl,-Bdynamic";;
+      macosx)
+        for lib in $LIBS; do
+          if [[ $lib == lib* ]]; then
+            archive="$lib.a"
+          else
+            archive="lib$lib.a"
+          fi
+          CCLIB="$CCLIB $(pkg-config $lib --variable libdir)/$archive"
+        done;;
+      *)
+        echo "No known mixed compilation flags for '$OS'" >&2
+        exit 1
+    esac;;
+
+  *)
+    echo "Invalid link mode '$LINK_MODE'" >&2
+    exit 2
+esac
+
+echo '('
+echo ' -linkall'
+for f in $FLAGS; do echo " $f"; done
+for f in $CCLIB; do echo " -cclib $f"; done
+echo ')'
