2784303e4f3457617abceaab5671615eb5122b52
Pierre Villemot <pierre.villemot@ocamlpro.com>
2024-03-19 11:49:33 +0100

    Saved the boolean model before resetting decisions
    The call `SAT.reset_decisions` may erase part of the boolean models in
    `get_value`. For sake of efficiency, we save the boolean model before
    resetting decisions.

--- src/lib/reasoners/sat_solver_util.ml
+++ src/lib/reasoners/sat_solver_util.ml
@@ -102,16 +102,14 @@ let assert_model (type a) (module SAT : S with type t = a) env mdl =
     mdl.Models.model;
   check (module SAT) env
 
-(* [get_value_in_boolean_model env e] retrieves the assignment of the boolean
-   expression [e] in the last boolean model of [env].
+(* [get_value_in_boolean_model bmdl e] retrieves the assignment of the boolean
+   expression [e] in the boolean model [bmdl].
 
-   @return [None] if the formula [e] hasn't been assigned by the SAT
-           solver. *)
-let get_value_in_boolean_model (type a) (module SAT : S with type t = a) env e =
+   @return [None] if the formula [e] hasn't been assigned by the SAT solver. *)
+let get_value_in_boolean_model bmdl e =
   match Expr.type_info e with
   | Ty.Tbool ->
     begin
-      let bmodel = SAT.get_boolean_model env in
       Stdcompat.List.find_map
         (fun lit ->
            if Expr.equal e lit then
@@ -121,20 +119,20 @@ let get_value_in_boolean_model (type a) (module SAT : S with type t = a) env e =
            else
              None
         )
-        bmodel
+        bmdl
     end
   | _ -> invalid_arg "get_value_in_boolean_model"
 
-(* [get_value_in_model mdl e] tries to retrieve the value of the expression
-   [e] in the model [mdl]. For boolean expressions, we retrieve their value
-   in the last boolean model of [env]. *)
-let get_value_in_model (type a) (module SAT : S with type t = a) env mdl e =
+(* [get_value_in_model mdl bmdl e] tries to retrieve the value of the expression
+   [e] in the first-order model [mdl]. For boolean expressions, we retrieve
+   their value in the boolean model [bmdl]. *)
+let get_value_in_model mdl bmdl e =
   let { Expr.f; xs; ty; _ } = Expr.term_view e in
   match f, ty with
   | _, Ty.Tbool ->
     (* Values of boolean expressions are decided by the SAT solver. *)
     Option.map Expr.Core.of_bool @@
-    get_value_in_boolean_model (module SAT) env e
+    get_value_in_boolean_model bmdl e
 
   | Symbols.Name name, _ ->
     let arg_tys = List.map Expr.type_info xs in
@@ -149,7 +147,10 @@ let get_value (type a) (module SAT : S with type t = a) env l =
   (* HOTFIX for SatML: we can assert new formula in [env.satml] only at the
      level of decision [0]. After performing [unsat], the decision level isn't
      necessary [0] and the SMT statement `get-value` can be called only in
-     SAT mode, so we have to reset all the decisions of the solver here. *)
+     SAT mode, so we have to reset all the decisions of the solver here.
+     Before resetting the decisions, we save the boolean model for sake
+     of optimization. *)
+  let bmdl = SAT.get_boolean_model env in
   SAT.reset_decisions env;
   assert_model (module SAT) env mdl;
   (* First, we attempt to retrieve each model term in the last computed
@@ -161,7 +162,7 @@ let get_value (type a) (module SAT : S with type t = a) env l =
   let l =
     List.map
       (fun e ->
-         match get_value_in_model (module SAT) env mdl e with
+         match get_value_in_model mdl bmdl e with
          | Some v -> Stdcompat.Either.Left v
          | None ->
            (* For each expression [e] of the list [l], we assert an equality
@@ -189,13 +190,14 @@ let get_value (type a) (module SAT : S with type t = a) env l =
   try
     check (module SAT) env;
     let* mdl = SAT.get_model env in
+    let bmdl = SAT.get_boolean_model env in
     let values =
       List.map
         (fun v ->
            match v with
            | Stdcompat.Either.Left v -> v
            | Stdcompat.Either.Right name ->
-             match get_value_in_model (module SAT) env mdl name with
+             match get_value_in_model mdl bmdl name with
              | Some v -> v
              | None ->
                (* The model generation has to produce a value for each
@@ -208,9 +210,10 @@ let get_value (type a) (module SAT : S with type t = a) env l =
   with Unsat ex -> raise_notrace (Wrong_model ex)
 
 let get_assignment (type a) (module SAT : S with type t = a) env =
+  let bmdl = SAT.get_boolean_model env in
   List.map
     (fun e ->
-       match get_value_in_boolean_model (module SAT) env e with
+       match get_value_in_boolean_model bmdl e with
        | Some true -> True
        | Some false -> False
        | None -> Unknown
