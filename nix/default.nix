6dc70464a75bfcad57b6858b98e758577beaa325
Pierre Villemot <pierre.villemot@ocamlpro.com>
2024-08-08 13:33:53 +0200

    Use `dynamic_include` to include the generated file dune.inc
    This PR uses the new feature `dynamic_include` to include our generated
    file `dune.inc`.
    
    Pros:
    - We do not upstream `dune.inc` anymore!
    - The output of `dune.inc` depends on the plateform. This is the main
      motivation of this PR because Windows does not use the convention for
      path.
    
    Cons:
    - We need `Dune > 3.14`
    - We cannot use the stanza `subdir` in `dune.inc`, which means we have
      to ensure we do not use twice the same name for two tests even if
      there are not in the same directory. See issue
      https://github.com/ocaml/dune/issues/10405

--- nix/default.nix
+++ nix/default.nix
@@ -4,7 +4,7 @@ import sources.nixpkgs {
   overlays = [
     (_: pkgs: { inherit sources; })
     (_: pkgs: {
-      ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14.overrideScope' (self: super: {
+      ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14.overrideScope (self: super: {
         pp_loc = pkgs.callPackage ./pp_loc.nix { };
         ocplib-simplex = pkgs.callPackage ./ocplib-simplex.nix { };
         dolmen = pkgs.callPackage ./dolmen.nix { };
