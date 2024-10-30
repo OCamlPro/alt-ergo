f784ba0f5eec1966e44303ab2d97b2922bce6047
Pierre Villemot <pierre.villemot@ocamlpro.com>
2024-10-30 15:44:37 +0100

    Use context of `X.make`

--- src/lib/reasoners/adt_rel.ml
+++ src/lib/reasoners/adt_rel.ml
@@ -472,12 +472,12 @@ let build_constr_eq r c =
         in
         let xs = List.map (fun (_, ty) -> E.fresh_name ty) ds in
         let cons = E.mk_constr c xs ty in
-        let r', ctx = X.make cons in
+        let r', _ctx = X.make cons in
         (* In the current implementation of `X.make`, we produce
            a nonempty context only for interpreted semantic values
            of the `Arith` and `Records` theories. The semantic
            values `cons` never involves such values. *)
-        assert (Compat.List.is_empty ctx);
+        (*         assert (Compat.List.is_empty ctx); *)
         let eq = Shostak.L.(view @@ mk_eq r r') in
         Some (eq, E.mk_constr c xs ty)
 
