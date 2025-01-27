9e890f2294388cd6e9b6746b86195257f8f8e0d4
Pierre Villemot <pierre.villemot@ocamlpro.com>
2025-01-27 12:11:08 +0100

    Remove useless comment

--- src/lib/reasoners/arrays_rel.ml
+++ src/lib/reasoners/arrays_rel.ml
@@ -46,7 +46,6 @@ module LR = Uf.LX
 let src = Logs.Src.create ~doc:"Arrays_rel" __MODULE__
 module Log = (val Logs.src_log src : Logs.LOG)
 
-(* map get |-> { set } des associations (get,set) deja splites *)
 module Tmap = struct
   include E.Map
   let update t a mp =
