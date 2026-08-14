7dee48621a0c1d47d472f82c4875ae7f2b996f7b
Pierre Villemot <pierre.villemot@ocamlpro.com>
2026-08-14 12:58:59 +0200

    Do not erase the locked switch at end of the script

--- rsc/extra/generate_lock_file.sh
+++ rsc/extra/generate_lock_file.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
-set -e
-trap cleanup EXIT
+set -euo pipefail
+export LOCKED_SWITCH="alt-ergo-locked"
 export OPAMYES=true
-export OPAMSWITCH="$LOCK_SWITCH"
+export OPAMSWITCH="$LOCKED_SWITCH"
 
-function cleanup() {
-  opam switch remove "$LOCK_SWITCH"
+function check_switch_available() {
+  opam list >/dev/null 2>&1
 }
 
-opam switch create "$LOCK_SWITCH" 5.4.1 --no-switch
+if check_switch_available; then
+  cat << EOF
+The switch $LOCKED_SWITCH already exists. Please remove it to run the script
+or override the value of the environment variable \$LOCKED_SWITCH.
+EOF
+  exit 1
+fi
+
+opam switch create "$LOCKED_SWITCH" 5.4.1 --no-switch
 
 opam install --deps-only --with-test --assume-depexts \
   ./alt-ergo-lib.opam \
